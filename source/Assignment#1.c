b1d69e99354c3a850f52d67aa28333f9fa92853a
Leandro Ebner <leandro.ebner@outlook.de>
2024-06-06 18:40:39 +0200

    renamed structure #2

---
+++ source/Assignment#1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int rows, collumns, i, j;
+
+int main(){
+  
+  printf("Insert amount of row(s):\n");
+  scanf("%d", &rows);
+  printf("Received %d row(s).\nInsert amount of collumn(s):\n", rows);
+  scanf("%d", &collumns);
+  printf("Received %d collumn(s).\n\n", collumns);
+
+  int array[rows];
+  
+  for(i=0; i < rows; i++){
+    array[i] = i+1;}
+  
+  for(j=0; j < collumns; j++){
+    for(i=0; i < rows; i++){printf("%d  ", array[i]*(j+1));}
+    printf("\n");}
+  
+}
