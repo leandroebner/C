dbc4e821b1cb44ad530692edda912845a930869c
Leandro Ebner <leandro.ebner@outlook.de>
2024-06-07 17:28:46 +0200

    added nonfunctional ass4 code

---
+++ source/assignment#3.c
@@ -0,0 +1,41 @@
+// Leandro Ebner 2024
+// Assignment #4: A simple calculator
+
+#include <stdio.h>
+
+float opr1, opr2;
+char optr;
+
+int main(){
+    
+    printf("Insert operator...\n");
+    scanf("%c", &optr);
+    printf("Received: [%c]! Insert Operand 1...\n", optr);
+    scanf("%f", &opr1);
+    printf("Received: [%f]! Insert Operand 2...\n", opr1);
+    scanf("%f", &opr2);
+    printf("Received: [%f]! \n", opr2);
+    calculate('+', opr1, opr2);
+    
+}
+
+float calculate(char optr, float opr1, float opr2){
+  
+  switch(optr){
+    case '+':
+      return opr1 + opr2;
+      break;
+    case '-':
+      return opr1 - opr2;
+      break;
+    case '*':
+      return opr1 * opr2;
+      break;
+    case '/':
+      return opr1 / opr2;
+      break;
+    //default: printf("Syntax Error: Invalid Operator");
+  }
+}
+
+
